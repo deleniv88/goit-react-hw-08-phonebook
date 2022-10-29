f52208f666ea9f0ff36d85a825e2592209cd955b
Andrew <deleniv08@gmail.com>
2022-10-29 18:31:31 +0200

    new

--- src/components/App.js
+++ src/components/App.js
@@ -36,6 +36,21 @@ class App extends Component {
     }));
   };
 
+  componentDidMount(){
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if(parsedContacts){
+      this.setState({contacts:parsedContacts})
+    }
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.contacts !== prevState.contacts){
+      localStorage.setItem('contacts',JSON.stringify(this.state.contacts))
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const normalizedFilter = this.state.filter.toLowerCase()
